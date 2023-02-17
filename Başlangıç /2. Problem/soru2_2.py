b24d7eb59f80c4622341fda077cdce557338d36f
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-17 16:44:27 +0300

    Update soru2_2.py

--- Başlangıç /2. Problem/soru2_2.py
+++ Başlangıç /2. Problem/soru2_2.py
@@ -1,1 +1,9 @@
 
+"""
+Kullanıcıdan alınan kısa kenar ve uzun kenar ile dikdörtgenin alanını hesaplayan programı yazın. (A*B)
+"""
+
+kisa_kenar = float(input("Lütfen kısa kenarın uzunluğunu giriniz: ")
+uzun_kenar = float(input("Lütfen uzun kenarın uzunluğunu giriniz: ")
+dikdortgen_alan = kisa_kenar * uzun_kenar
+print(dikdortgen_alan)
