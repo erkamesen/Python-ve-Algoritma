f58383768ffbd1baedd9ad2901ec8185b502f2d4
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-18 00:01:37 +0300

    Create soru9_2.py

---
+++ Başlangıç /9. Problem/soru9_2.py
@@ -0,0 +1,22 @@
+"""
+'Kullanıcıdan 2 sayı alıp 1 den 10 a kadar çarpımlarını 2 tab boşlukla("\t") aynı tabloda gösteren programı tasarla.'
+"""
+
+sayi1 = int(input("Lütfen bir sayı giriniz: ") -> 5
+sayi2 = int(input("Lütfen bir sayı giriniz: ") -> 10
+for i in range(1,11):
+  print(f"{i} x {sayi1} = {i*sayi1}\t\t{i} x {sayi2} = {i*sayi2} ") 
+  i += 1
+
+"""
+1 x 5 = 5		  1 x 10 = 10
+2 x 5 = 10		2 x 10 = 20
+3 x 5 = 15		3 x 10 = 30
+4 x 5 = 20		4 x 10 = 40
+5 x 5 = 25		5 x 10 = 50
+6 x 5 = 30		6 x 10 = 60
+7 x 5 = 35		7 x 10 = 70
+8 x 5 = 40		8 x 10 = 80
+9 x 5 = 45		9 x 10 = 90
+10 x 5 = 50		10 x 10 = 100
+"""
