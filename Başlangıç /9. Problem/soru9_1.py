44563658a2ae8fc139665e604ea97fe6d3222e67
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-18 00:01:18 +0300

    Create soru9_1.py

---
+++ Başlangıç /9. Problem/soru9_1.py
@@ -0,0 +1,22 @@
+"""
+'Kullanıcıdan alınan sayının 1 den 10 a kadar olan çarpımlarını ekrana yazdıran programı tasarla.'
+"""
+
+
+sayi = int(input("Lütfen bir sayı giriniz: ") -> 12
+for i in range(1,11):
+  print(f"{i} x {sayi} = {i*sayi}") 
+  i += 1
+
+"""
+1 x 12 = 12
+2 x 12 = 24
+3 x 12 = 36
+4 x 12 = 48
+5 x 12 = 60
+6 x 12 = 72
+7 x 12 = 84
+8 x 12 = 96
+9 x 12 = 108
+10 x 12 = 120
+"""
