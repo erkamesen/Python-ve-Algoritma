5e10e3efc9b87cc38777acf8d6a088642ccd32d7
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-17 16:07:09 +0300

    Create soru1_1.py

---
+++ Başlangıç /1. Problem/soru1_1.py
@@ -0,0 +1,17 @@
+"""
+Verilen tabloyu oluşturun.-Boşluklar için 2 tane tab("\t") kullanabilirsiniz.-
+
+Sayı  Karesi
+1     1
+2     4
+3     9
+4     16
+5     25
+"""
+
+print("Sayı \t\t Karesi")
+print("1\t\t"+str(1**2))
+print("2\t\t"+str(2**2))
+print("3\t\t"+str(3**2))
+print("4\t\t"+str(4**2))
+print("5\t\t"+str(5**2))
