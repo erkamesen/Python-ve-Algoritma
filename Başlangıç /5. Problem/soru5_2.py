3110ee2ea7eea08b7b2a372d7af77acd3f6be988
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-17 20:28:41 +0300

    Create soru5_2.py

---
+++ Başlangıç /5. Problem/soru5_2.py
@@ -0,0 +1,11 @@
+""" 
+'Kullanıcıdan 3 farklı sayı alıp bunların çarpımının toplamına bölümünü ekrana çıktı olarak veren programı tasarla.'
+"""
+
+sayi1 = float(input("Lütfen birinci sayıyı giriniz: ")
+sayi2 = float(input("Lütfen ikinci sayıyı giriniz: ")
+sayi3 = float(input("Lütfen üçüncü sayıyı giriniz: ")
+carpim = sayi1 * sayi2 * sayi3
+toplam = sayi1 + sayi2 + sayi3
+oran = carpim / toplam
+print("Sonuç :", oran)
