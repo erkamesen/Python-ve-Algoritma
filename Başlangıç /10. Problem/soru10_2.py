5948aa9c570a00acb1914e1467b49cb3b6e92dcb
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-18 00:44:40 +0300

    Create soru10_2.py

---
+++ Başlangıç /10. Problem/soru10_2.py
@@ -0,0 +1,30 @@
+"""
+'Kullanıcıdan alınan 10 sayının karesini sırasıyla ekrana yazdıran programı tasarla'
+"""
+
+
+for i in range(10):
+  sayi = int(input("Lütfen karesi alınacak sayıyı giriniz: ") -> 3, 4, 2, 5, 8, 11, 15, 6, 10, 9
+  print(f"Sayının Karesi: {sayi**2}"
+"""
+Lütfen karesi alınacak sayıyı giriniz: 3
+Sayının Karesi: 9
+Lütfen karesi alınacak sayıyı giriniz: 4
+Sayının Karesi: 16
+Lütfen karesi alınacak sayıyı giriniz: 2
+Sayının Karesi: 4
+Lütfen karesi alınacak sayıyı giriniz: 5
+Sayının Karesi: 25
+Lütfen karesi alınacak sayıyı giriniz: 8
+Sayının Karesi: 64
+Lütfen karesi alınacak sayıyı giriniz: 11
+Sayının Karesi: 121
+Lütfen karesi alınacak sayıyı giriniz: 15
+Sayının Karesi: 225
+Lütfen karesi alınacak sayıyı giriniz: 6
+Sayının Karesi: 36
+Lütfen karesi alınacak sayıyı giriniz: 10
+Sayının Karesi: 100
+Lütfen karesi alınacak sayıyı giriniz: 9
+Sayının Karesi: 81
+"""
