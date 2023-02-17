34a3cfc4230920cea76fd84b415f4a4953669cbf
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-17 16:07:53 +0300

    Create soru2_2.py

---
+++ Başlangıç /1. Problem/soru2_2.py
@@ -0,0 +1,17 @@
+"""
+Verilen tabloyu oluşturun.-Boşluklar için 2 tane tab("\t") kullanabilirsiniz.-
+
+Sayı  Küpü
+1     1
+2     8
+3     27
+4     64
+5     125
+"""
+
+print("Sayı \t\t Küpü")
+print("1\t\t"+str(1**3))
+print("2\t\t"+str(2**3))
+print("3\t\t"+str(3**3))
+print("4\t\t"+str(4**3))
+print("5\t\t"+str(5**3))
