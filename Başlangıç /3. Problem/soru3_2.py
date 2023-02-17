40dcd4cf3350119b527a17b10fe0a64d16c10fc2
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-17 19:37:29 +0300

    Create soru3_2.py

---
+++ Başlangıç /3. Problem/soru3_2.py
@@ -0,0 +1,11 @@
+"""
+Kullanıcıdan ders bilgisi ve not bilgisini alıp ekrana alttaki formatta gösterecek programı yazın.
+
+    # Diferansiyel Denklemler dersinden tam olarak 100 aldım.
+
+"""
+
+ders = input("Lütfen bir ders giriniz: ")
+not = input("Lütfen notunuzu giriniz: ")
+print(f"{} dersinden tam olarak {} aldım.")
+print("{} dersinden tam olarak {} aldım.".format(ders, not))
