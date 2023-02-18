af57a8f806d7e50ac14c50ffab49bae5c5840f0e
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-18 15:37:41 +0300

    Create soru12_2.py

---
+++ Başlangıç /12. Problem/soru12_2.py
@@ -0,0 +1,23 @@
+"""
+'Aşağıdaki şekili çıktı olarak veren programı tasarlayın.
+
+     *
+    **
+   ***
+ *****
+******
+
+'
+"""
+
+j = 4
+for i in range(5):
+    print((" "*j)+(i+1)*"*")
+    j -=1
+"""
+    *
+   **
+  ***
+ ****
+*****
+"""
