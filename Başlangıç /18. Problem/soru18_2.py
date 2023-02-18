dd7e66f4c1c4451516401304b6225cb090365fe2
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 01:29:47 +0300

    Create soru18_2.py

---
+++ Başlangıç /18. Problem/soru18_2.py
@@ -0,0 +1,14 @@
+"""
+'kullanıcının adını, okul numarasını ve 3 ders bilgisi alıp dersleri liste şeklinde sözlükte saklayıp kullanıcıya gösteren programı tasarlayın.'
+"""
+
+
+bilgiler = {}
+dersler = []
+ad = input("Lütfen adınızı giriniz: ")
+okul_numarasi = input("Lütfen okul numarasınızı giriniz: ")
+for i in range(3):
+  dersler.append(input("Lütfen bir ders giriniz: "))
+bilgiler["Adı"] = ad
+bilgiler["Okul Numarası"]
+bilgiler["Alınan Dersler"] = dersler
