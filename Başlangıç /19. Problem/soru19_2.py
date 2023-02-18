e8532b2d1f28e0127eeb849d7d0b970603ddd671
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 01:36:41 +0300

    Create soru19_2.py

---
+++ Başlangıç /19. Problem/soru19_2.py
@@ -0,0 +1,15 @@
+"""
+'Kullanıcıdan 4 adet sayı bilgisi alıp aşağıdaki denklemde yerine koyan ve sonucu kullanıcıya gösteren programı tasarlayın.
+
+    d+a+2.a.b/d(4c+10)
+
+'
+"""
+
+
+a = int(input("Birinci sayı: ")
+b = int(input("İkinci sayı: ")
+c = int(input("Üçüncü sayı: ")
+d = int(input("Üçüncü sayı: ")
+sonuc = d+a+2*a*b/d(4c+10)
+print(sonuc)
