df967cb60ecc725fb9641ccc48c476d171c61c3d
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-18 21:12:43 +0300

    Create soru16_1.py

---
+++ Başlangıç /16. Problem/soru16_1.py
@@ -0,0 +1,12 @@
+"""
+'Kullanıcıdan alınan bir sayının compound(bileşik) operatörler ile işlemlerini ve sonuçlarını gösteren programı tasarlayın.
+(+=, -= vb.)'
+"""
+
+
+sayi = int(input("Lütfen bir sayı giriniz: "))
+print(sayi += 5) # sayi = sayi + 5
+print(sayi -= 5) # sayi = sayi - 5
+print(sayi /= 5) # sayi = sayi / 5
+print(sayi *= 5) # sayi = sayi * 5
+print(sayi %= 5) # sayi = sayi % 5
