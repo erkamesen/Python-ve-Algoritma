aaf81348e9ce082a9ad95cb8a271c7653d6c7eae
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 01:36:20 +0300

    Create soru19_1.py

---
+++ Başlangıç /19. Problem/soru19_1.py
@@ -0,0 +1,13 @@
+"""
+'Kullanıcıdan 3 adet sayı bilgisi alıp aşağıdaki denklemde yerine koyan ve sonucu kullanıcıya gösteren programı tasarlayın.
+
+    a+b+c.a/b(2a+3b)
+
+'
+"""
+
+a = int(input("Birinci sayı: ")
+b = int(input("İkinci sayı: ")
+c = int(input("Üçüncü sayı: ")
+sonuc = a+b+c*a/b(2a + 3b)
+print(sonuc)
