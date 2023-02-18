486aab5da0c14ae92d75371f3862860222917c1d
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-18 23:32:43 +0300

    Create soru17_2.py

---
+++ Başlangıç /17. Problem/soru17_2.py
@@ -0,0 +1,23 @@
+"""
+'Aşağıdaki şekili oluşturan programı tasarlayın. (sadece bir print() fonksiyonu kullanarak)
+
+A
+B  C
+D  E  F
+G  H  I  J  K
+L  M  N  O  P  Q
+R  S  T  U  V  W  X 
+
+' 
+"""
+
+print("A\nB\tC\nD\tE\tF\nG\tH\tI\tJ\tK\nL\tM\tN\tO\tP\tQ\nR\tS\tT\tU\tV\tW\tX")
+
+"""
+A
+B	 C
+D	 E	F
+G	 H	I	 J	K
+L	 M	N	 O	P  Q
+R	 S	T	 U	V	 W	X
+"""
