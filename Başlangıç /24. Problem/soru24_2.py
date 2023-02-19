8acf8613cf019a512330a3f52c71c107efca9970
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 16:32:03 +0300

    Create soru24_2.py

---
+++ Başlangıç /24. Problem/soru24_2.py
@@ -0,0 +1,19 @@
+"""
+'Kullanıcıdan alınan 2 si sayı biri operatör parametresine göre 2 sayı arasında işlem yapan fonksiyonu yazın ve programı tasarlayın.'
+"""
+
+
+def hesap(n1, n2, islem)
+  if islem == "+":
+    return n1+n2
+  elif islem == "-":
+    return n1-n2
+  elif islem == "*":
+    return num1*num2
+  elif islem == "/":
+    return num1/num2
+    
+num1 = int(input("Lütfen ilk sayıyı giriniz: "))
+num2 = int(input("Lütfen ikinci sayıyı giriniz: "))
+islem = input("Lütfen yapacağınız işlemi giriniz(+, -, *, /): )
+hesap(n1=num1, n2=num2, islem=islem)
