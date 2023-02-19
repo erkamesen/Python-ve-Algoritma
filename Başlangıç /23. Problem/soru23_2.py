8ae2a00de33c08562e7070769b509fe67c98226a
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 14:04:21 +0300

    Update soru23_2.py

--- Başlangıç /23. Problem/soru23_2.py
+++ Başlangıç /23. Problem/soru23_2.py
@@ -2,3 +2,16 @@
 'Kullanıcıdan bir gün bilgisi alıp hafta içi mi hafta sonu mu olduğunu ekrana çıktı olarak yazan programı tasarlayın.
 Eğer 7 günden farklı bir şey yazarsa program hata versin.'
 """
+
+
+hafta_ici = ["PAZARTESİ","SALI","ÇARŞAMBA","PERŞEMBE","CUMA"]
+hafta_sonu = ["CUMARTESİ","PAZAR"]
+
+gun = input("Lütfen bulunduğunuz günü girin: ").upper()
+
+if gun in hafta_ici:
+  print("Bugün hafta içi")
+elif gun in hafta_sonu:
+  print("Bugün hafta sonu")
+else:
+  print("Bu bir gün değil")
