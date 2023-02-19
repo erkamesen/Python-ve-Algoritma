d1b2168a466aed77cd8a7c83560d63e0ce18fbdc
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 19:19:17 +0300

    Create soru29_1.py

---
+++ Başlangıç /29. Problem/soru29_1.py
@@ -0,0 +1,10 @@
+"""
+'Kullanıcıdan alınan sayının negatif mi pozitif mi olduğunu kontrol eden programı tasarlayın.'
+"""
+
+
+sayi = int(input("Lütfen kontrol edilecek sayıyı giriniz: ")
+if sayi < 0:
+  print(f"{sayi} Negatif")
+elif sayi > 0:
+  print(f"{sayi} Pozitif")
