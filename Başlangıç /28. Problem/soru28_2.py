39df3bfacafd6c4b7cfbc4336e239ec915b91c0e
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 19:14:38 +0300

    Create soru28_2.py

---
+++ Başlangıç /28. Problem/soru28_2.py
@@ -0,0 +1,9 @@
+"""
+'Kullanıcıdan bir sayıp alıp sayının küpünü kullanıcıya gösteren programı tasarlayın.'
+"""
+
+
+
+sayi = int(input("Karesi alınacak sayıyı giriniz: "))
+sayi **= 3
+print(f"Sayının küpü: {sayi}")
