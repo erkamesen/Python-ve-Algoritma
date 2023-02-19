a4b2b7975cc838bbae878b10d9664e835ed6f360
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 17:14:17 +0300

    Create soru26_2.py

---
+++ Başlangıç /26. Problem/soru26_2.py
@@ -0,0 +1,9 @@
+"""
+'Kullanıcıdan bir sayı alıp çift mi tek mi olduğunu kontrol edip çiftse karesini tekse küpünü alıp ekranda gösteren programı tasarlayın.'
+"""
+
+sayi = int(input("Lütfen bir sayı giriniz: "))
+if sayi %2==0:
+  print(sayi**2=)
+else:
+  print(sayi**3)
