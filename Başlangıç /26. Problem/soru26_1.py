7de1d09b90ca2adcca4c4c85625606ea490ec3c9
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 17:14:00 +0300

    Create soru26_1.py

---
+++ Başlangıç /26. Problem/soru26_1.py
@@ -0,0 +1,9 @@
+"""
+'Kullanıcıdan bir sayı alıp çift mi tek mi olduğunu ekranda gösteren programı tasarlayın.'
+"""
+
+sayi = int(input("Lütfen bir sayı giriniz: "))
+if sayi %2==0:
+  print("Çift")
+else:
+  print("Tek")
