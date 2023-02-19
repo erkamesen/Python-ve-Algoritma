eb5d72eb973d114690930862ddc2477a85bd4647
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 19:14:13 +0300

    Create soru28_1.py

---
+++ Başlangıç /28. Problem/soru28_1.py
@@ -0,0 +1,7 @@
+"""
+'Kullanıcıdan bir sayıp alıp sayının karesini kullanıcıya gösteren programı tasarlayın.'
+"""
+
+sayi = int(input("Karesi alınacak sayıyı giriniz: "))
+sayi **= 2
+print(f"Sayının karesi: {sayi}")
