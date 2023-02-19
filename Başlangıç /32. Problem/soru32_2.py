25d477e9666b9b21a0fd5bfd861c91873625c761
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 21:34:54 +0300

    Create soru32_2.py

---
+++ Başlangıç /32. Problem/soru32_2.py
@@ -0,0 +1,15 @@
+"""
+'kullanıcıdan 2 adet sayı alıp parametre olarak 2 sayı alan ve bu sayıların küçük olanını döndüren fonksiyonu tasarlayın.'
+"""
+
+
+
+def min_sayi(n1, n2):
+  if n1 < n2:
+    return n1
+  else:
+    return n2
+
+sayi1 = int(input("Lütfen ilk sayıyı giriniz: "))
+sayi2 = int(input("Lütfen ikinci sayıyı giriniz: "))
+print(min_sayi(n1=sayi1, n2=sayi2))
