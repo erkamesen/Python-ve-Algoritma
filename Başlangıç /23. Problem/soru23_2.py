cad48682cb27b49647ad91a1feac741fcce8cf9e
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-19 14:04:09 +0300

    Create soru23_2.py

---
+++ Başlangıç /23. Problem/soru23_2.py
@@ -0,0 +1,4 @@
+"""
+'Kullanıcıdan bir gün bilgisi alıp hafta içi mi hafta sonu mu olduğunu ekrana çıktı olarak yazan programı tasarlayın.
+Eğer 7 günden farklı bir şey yazarsa program hata versin.'
+"""
