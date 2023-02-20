dd77cbf7fdcece8d4185f8ca8e6c4653c5768ec7
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-20 22:01:27 +0300

    Create soru37_1.py

---
+++ Başlangıç /37. Problem/soru37_1.py
@@ -0,0 +1,8 @@
+"""
+'Kullanıcıdan alınan 2 sayının küplerinin toplamını bulun programı tasarlayın.'
+"""
+
+
+sayi1 = int(input("Lütfen ilk sayıyı giriniz: "))
+sayi2 = int(input("Lütfen ikinci sayıyı giriniz: "))
+print(sayi1**3 + sayi2**3)
