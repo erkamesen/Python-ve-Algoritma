ce3de500bc5d8bb572f27d7aa5cbf245fe2184e8
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-20 23:55:08 +0300

    Create soru38_2.py

---
+++ Başlangıç /38. Problem/soru38_2.py
@@ -0,0 +1,10 @@
+"""
+'Kullanıcıdan alınan dereceyi radyana dönüştüren programı (math modülü yardımı ile) tasarlayın.'
+"""
+
+
+
+import maths
+deg = int(input("Lütfen çevirmek için radyan giriniz: "))
+rad = math.radians(rad)
+print(rad)
