0d74843183be1a74e4dfd545bae8ce60cfb8eb4e
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-20 21:54:32 +0300

    Create soru36_1.py

---
+++ Başlangıç /36. Problem/soru36_1.py
@@ -0,0 +1,6 @@
+"""
+'kullanıcıdan bir cümle alıp alınan bu cümleyi ilk harfi büyük şekilde kullanıcıya gösteren programı tasarlayın.'
+"""
+
+cumle = input("lütfen bir cümle giriniz: ").capitalize()
+print(cumle)
