fb13488ece09f3eae441f448dc07a8eb0e837d3f
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-22 00:52:25 +0300

    Create soru43_2.py

---
+++ Başlangıç /43. Problem/soru43_2.py
@@ -0,0 +1,39 @@
+"""
+'1 den 100 e kadar 4 er 4 er sayan programı tasarla.'
+"""
+
+
+for sayi in range(1,101):
+  if sayi%4 == 0 :
+    print(sayi)
+  else:
+    continue
+    
+    
+"""
+4
+8
+12
+16
+20
+24
+28
+32
+36
+40
+44
+48
+52
+56
+60
+64
+68
+72
+76
+80
+84
+88
+92
+96
+100
+"""
