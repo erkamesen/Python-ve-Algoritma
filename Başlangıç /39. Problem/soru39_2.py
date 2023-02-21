aae7e5a5a9cdc3adf1e49413d077bddc76078135
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-21 22:46:33 +0300

    Create soru39_2.py

---
+++ Başlangıç /39. Problem/soru39_2.py
@@ -0,0 +1,18 @@
+"""
+'Kullanıcıdan 5 adet sayı alıp bunu tuple da saklayan, ardından bu tuple ı parametre
+olarak alıp listedeki sayıların toplamını döndüren fonksiyonu ve programı tasarlayın.'
+"""
+
+
+def tuple_toplayici(l):
+  s = 0
+  for sayi in l:
+    s += int(sayi):
+  return s
+  
+sayilar = ()
+
+for i in range(5):
+  sayilar += (int(input("Lütfen bir sayı giriniz: "), )
+
+print(tuple_toplayici(l=sayilar))
