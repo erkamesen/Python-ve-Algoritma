808e82a696ccd53e624b747f25e5bd81301b4d86
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-22 13:51:08 +0300

    Create soru49_2.py

---
+++ Başlangıç /49. Problem/soru49_2.py
@@ -0,0 +1,8 @@
+"""
+'Kullanıcıdan alınan saniyeyi saate çeviren programı tasarlayın.'
+"""
+
+
+saniye = float(input("Lütfen çevirmek istediğiniz saniyeyi giriniz: ")) # 452322
+saat = saniye/3600
+print(f"{saniye} sn tam olarak {saat} saat ediyor.") # 1256.45
