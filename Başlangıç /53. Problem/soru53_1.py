346558938900ff44ae53ddc987d44dcc708d2c17
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-22 15:21:32 +0300

    Create soru53_1.py

---
+++ Başlangıç /53. Problem/soru53_1.py
@@ -0,0 +1,11 @@
+"""
+'Kullanıcıdan yaşını alıp saniyeye çevirerek kullancııya gösteren programı tasarlayın.'
+"""
+
+
+yas = float(input("Lütfen yaşınızı giriniz: ")) # 27
+
+saniye_yas = yas*360*24*60*60 
+print(f"Şu zamana kadar tam olarak {saniye_yas} saniye yaşadınız.")
+
+# Şu zamana kadar tam olarak 839808000 saniye yaşadınız.
