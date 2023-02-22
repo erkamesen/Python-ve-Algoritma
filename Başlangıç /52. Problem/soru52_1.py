395f5bb70193d39212dbc330c4a73690c300687f
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-22 15:12:02 +0300

    Create soru52_1.py

---
+++ Başlangıç /52. Problem/soru52_1.py
@@ -0,0 +1,11 @@
+"""
+'Kullanıcıdan alınan cümleyi ters çevirip kullanıcıya gösteren programı tasarlayın.'
+"""
+
+
+cumle = input("Lütfen çevirme kiçin bir cümle giriniz: ") # Python yüksek seviye ve nesne yöenlimli bir dildir.
+ters_cumle = cumle[::-1]
+
+print(ters_cumle) 
+
+# .ridlid rib ilmilneöy ensen ev eyives kesküy nohtyP
