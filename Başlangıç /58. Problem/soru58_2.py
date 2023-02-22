af8d77980148500881fbb464b49242d35520e274
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-22 21:47:19 +0300

    Create soru58_2.py

---
+++ Başlangıç /58. Problem/soru58_2.py
@@ -0,0 +1,8 @@
+"""
+'Kullanıcıdan alınan isim ve soyisimi baş harfleri büyütülecek şekilde tam halde kullanıcıya gösteren programı tasarlayın.'
+"""
+
+
+isim = input("Lütfen isminizi giriniz: ").capitalize() # erkam
+soyisim = input("Lütfen soyisminizi giriniz: ").capitalize() # esen
+print(isim, soyisim) # Erkam Esen
