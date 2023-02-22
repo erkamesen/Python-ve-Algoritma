2cd972217589de6e0b3f4a0c34c5840d1b5bb9e1
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-22 13:59:53 +0300

    Create soru50_1.py

---
+++ Başlangıç /50. Problem/soru50_1.py
@@ -0,0 +1,10 @@
+"""
+'Kullanıcıdan alınan taban uzunluğu ve yükseklik ile üçgenin alanını bulan programı tasarlayın. alan=(taban uzunluğu*yükseklik)/2'
+"""
+
+
+taban_uzunlugu = float(input("Lütfen üçgenin taban uzunluğunu giriniz: ")) # 10
+yukseklik = float(input("Lütfen üçgenin yüksekliğini giriniz: ")) # 30
+
+alan = (taban_uzunlugu*yukseklik)/2
+print(alan) # 150
