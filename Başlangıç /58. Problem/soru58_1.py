dd3a2c72c1680f97cb4aea31ff722770fe27da0e
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-22 21:46:59 +0300

    Create soru58_1.py

---
+++ Başlangıç /58. Problem/soru58_1.py
@@ -0,0 +1,7 @@
+"""
+'Kullanıcıdan alınan isim ve soyisimi tam halde kullanıcıya gösteren programı tasarlayın.'
+"""
+
+isim = input("Lütfen isminizi giriniz: ") # erkam
+soyisim = input("Lütfen soyisminizi giriniz: ") # esen
+print(isim, soyisim) # erkam esen
