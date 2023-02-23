d5135646334accae15f37692b9821174ba5de719
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-02-23 15:06:23 +0300

    Create soru62_1.py

---
+++ Başlangıç /62. Problem/soru62_1.py
@@ -0,0 +1,24 @@
+"""
+'Kullanıcıdan aldığı isim, babasının ismi, yaş ve telefon bilgileri ile aşağıdaki tabloyu oluşturan programı tasarlayın.
+
+Adı:           ...
+Baba Adı:      ...
+Yaş:           ...
+Telefon:       ...
+
+(2 alan arasında ilk alandaki yazılar dahil toplam 15 karakter uzunlukta boşluk var.)' 
+"""
+
+
+ad = input("Adınız: ") # Erkam
+baba_adi = input("Babanızın Adı: ") # Muzaffer
+yas = input("Yaşınız: ") # 27
+telefon = input("Telefon Numarası: ") # 123123123
+
+print("Adı:"+11*" "+ad+"\nBaba Adı:"+6*" "+baba_adi+"\nYaş:"+11*" "+yas+"\nTelefon:"+7*" "+telefon)
+"""
+Adı:           Erkam
+Baba Adı:      Muzaffer
+Yaş:           27
+Telefon:       123123123
+"""
