e6a5302da56c42bf584f0cc8d6fcf9134caa0c23
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-03-13 22:14:58 +0300

    Update soru67.1.py

--- Başlangıç /67. Problem/soru67.1.py
+++ Başlangıç /67. Problem/soru67.1.py
@@ -1,3 +1,15 @@
 """
-ss
+'Kullanıcıdan alınan 2 sayının toplamlarını gösteren programı tasarlayın.
+! Sayılar pozitif ve 50 den küçük olmalı eğer koşulları sağlamıyorsa hata döndür.'
 """
+
+
+sayi1 = int(input("Lütfen 0-50 aralığında bir sayı giriniz: ")) # 42
+sayi2 = int(input("Lütfen 0-50 aralığında bir sayı giriniz: ")) # 53
+
+if sayi1<0 or sayi1>49 or sayi2<0 or sayi2>49:
+  print("Belirtilen aralıkta bir sayı girmediniz.")
+else:
+  print(sayi1 + sayi2)
+
+# Belirtilen aralıkta bir sayı girmediniz.
