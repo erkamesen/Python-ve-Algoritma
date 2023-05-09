4a39457a6d29cb776ee0338d09ede98f5fea9900
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-05-09 11:39:07 +0300

    Update and rename soru7_2.py to soru7.2.py

--- Başlangıç /7. Problem/soru7.2.py
+++ Başlangıç /7. Problem/soru7.2.py
@@ -6,7 +6,7 @@ Verilen 3 kümenin kesişimini çıktı olarak gösteren programı tasarlayın.'
 """
 
 set1 = {5,2,4,6,7,1}
-set2 = {5,3,11,1}
+set2 = {5,3,11}
 set3 = {4,5,12,2,1,0}
 new_set = set1 & set2 & set3
 # {5, 1}
